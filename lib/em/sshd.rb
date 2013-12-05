ac063d8ec427024a61e0c11b52162a22b06b403b
Robin Doer <robin@robind.de>
2013-12-05 12:28:35 +0100

    Initial commit

---
+++ lib/em/sshd.rb
@@ -0,0 +1,9 @@
+require "eventmachine"
+
+require "em/sshd/version"
+
+module EventMachine
+  module Sshd
+    # Your code goes here...
+  end
+end
---
+++ lib/em/sshd/version.rb
@@ -0,0 +1,5 @@
+module EventMachine
+  module Sshd
+    VERSION = "0.0.1"
+  end
+end
